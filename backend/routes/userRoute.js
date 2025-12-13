88d9bc04936944569f258c75a2e2462e9431f9f5
Arpit Tripathi <arpit.tripathi0610@gmail.com>
2025-12-13 18:32:30 +0530

    added send-otp-api, verify-otp-api and input validation for signup,send-otp and verify-otp route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { upload } from '../cloudinaryConfig.js'; 
-import { signup } from '../controllers/userController.js';
+import { upload } from '../cloudinaryConfig.js';
+import { signup, sendOtp, verifyOtp } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/signup', upload.single('profile_picture'), signup);
+router.post('/send-otp', sendOtp);
+router.post('/verify-otp', verifyOtp);
 
 export default router;
